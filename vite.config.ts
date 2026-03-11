276ee85c4ab21381f85065bda81277477b8284a8
Snehin Adepu <23071a66c5@vnrvjiet.in>
2026-03-11 21:00:39 +0530

    Complete setup

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
